b042fb442dcc14f6aff93af4fed874cd2e2bf496
KarlaNunes <karla.julyana1@gmail.com>
2023-10-14 12:25:16 -0300

    :wrench: criação do requirements com venv ativada

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\taysa\Desktop\4 periodo\Integrador\venv
+set VIRTUAL_ENV=C:\Users\julya\Área de Trabalho\Pessoal\ADS\PI\Integrador1\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
