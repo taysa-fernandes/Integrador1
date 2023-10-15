6d6e6ee2af5cdc1dbcbbd71028e51615853b9c82
taysa-fernandes <taysamirelly34@gmail.com>
2023-10-15 11:59:58 -0300

    visualizar dieta

--- core/views.py
+++ core/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'core/index.html')
+def diario(request):
+    return render(request, 'core/visualizarDieta.html')
