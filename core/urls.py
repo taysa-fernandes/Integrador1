037e81e70d2ae59f8963c60c0c57b3f112569034
taysa-fernandes <taysamirelly34@gmail.com>
2023-10-17 20:10:26 -0300

    estilização da página

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from  core.views import index
+from  core.views import index,diario
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',index),
+    path('diario',diario)
 ]
