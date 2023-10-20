5588f77a3dc3982c7d37c74ae2559239ea3fff0e
taysa-fernandes <taysamirelly34@gmail.com>
2023-10-20 11:18:14 -0300

    finalização de esqueleto html

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from  core.views import index,dieta
+from  core.views import index,dieta,cadastrar_alimento
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',index),
-    path('dieta',dieta)
+    path('dieta',dieta),
+    path('cadastrar-alimento',cadastrar_alimento)
 ]
--- core/views.py
+++ core/views.py
@@ -4,3 +4,5 @@ def index(request):
     return render(request, 'core/index.html')
 def dieta(request):
     return render(request, 'core/visualizarDieta.html')
+def cadastrar_alimento(request):
+    return render(request, 'core/cadastrarAlimento.html')
