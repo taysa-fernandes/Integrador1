b27c96fc6ee63ddb5be2e28746676a80e8c4332a
taysa-fernandes <taysamirelly34@gmail.com>
2023-10-19 21:12:02 -0300

    registro de progresso esqueleto

--- core/views.py
+++ core/views.py
@@ -4,3 +4,5 @@ def index(request):
     return render(request, 'core/index.html')
 def dieta(request):
     return render(request, 'core/visualizarDieta.html')
+def progresso(request):
+    return render(request, 'core/registrarProgresso.html')
