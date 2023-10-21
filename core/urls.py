b750af1b340b049002865277953de0e822eed0d6
KarlaNunes <karla.julyana1@gmail.com>
2023-10-21 15:21:52 -0300

    :sparkles: nova view

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from  core.views import index
+from  core.views import index, meus_formularios
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',index),
+    path('meus-formularios/',meus_formularios),
 ]
--- core/views.py
+++ core/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'core/index.html')
+
+def meus_formularios(request):
+    return render(request, 'core/meus-formularios.html')
