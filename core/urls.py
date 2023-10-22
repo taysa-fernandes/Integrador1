ecfcb150a59e4778a9f8e8f0f08c80fe914f567d
taysa-fernandes <taysamirelly34@gmail.com>
2023-10-22 13:27:48 -0300

    ignore

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from  core.views import home, index, meus_formularios, novo_formulario, dieta, alimentos,cadastrar_paciente
+from  core.views import home, index, meus_formularios, novo_formulario, dieta, alimentos,cadastrar_paciente,progresso,diario
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,6 +11,6 @@ urlpatterns = [
     path('dieta',dieta),
     path('progresso',progresso, name="progresso"),
     path('diario',diario,name="diario"),
-    path('alimentos/', alimentos)
+    path('alimentos/', alimentos),
     path('cadastrar-paciente',cadastrar_paciente)
 ]
