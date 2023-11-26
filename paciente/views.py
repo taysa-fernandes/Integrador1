001075a4c83f4e4ce66d366f4193023d7aa9232e
taysa-fernandes <taysamirelly34@gmail.com>
2023-11-25 22:26:55 -0300

    correção de models de alimento e migrações

--- paciente/views.py
+++ paciente/views.py
@@ -1,4 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponseRedirect, render
+from django.urls import reverse_lazy
+from .models import Paciente
+from .forms import PacienteForm
+from django.views.generic import CreateView,UpdateView,ListView,DeleteView
 
-def cadastrar_paciente(request):
-    return render(request, 'paciente/cadastrarPaciente.html')
+class PacienteEditar(UpdateView):
+    model = Paciente
+    form_class = PacienteForm
+    template_name = 'paciente/cadastrarPaciente.html'
+    success_url = reverse_lazy('core/home.html')
+    pk_url_kwarg = 'id'
+    
+class PacienteDeletar(DeleteView):
+    model = Paciente
+    success_url = reverse_lazy('core/home.html')
+    pk_url_kwarg = 'id'
+    
+    def get(self, *args, **kwargs):
+        return self.delete(*args, **kwargs)
+    
+# class PacienteCriar(CreateView):
+#     model = Paciente
+#     form_class = PacienteForm
+#     template_name = 'paciente/cadastrarPaciente.html'
+#     success_url = reverse_lazy('core/home.html')
+
+def PacienteCriar(request):
+    if request.method == 'POST':
+        form = PacienteForm(request.POST)
+        if form.is_valid():
+            form.save() 
+            return HttpResponseRedirect('core/home.html') 
+    else:
+        form = PacienteForm()
+    return render(request, 'paciente/cadastrarPaciente.html', {'form': form})
+    
+class PacienteListar(ListView):
+    model = Paciente
+    template_name = 'core/home.html'
+    context_object_name = 'pacientes'
+
+# def cadastrar_paciente(request):
+#     return render(request, 'paciente/cadastrarPaciente.html')
