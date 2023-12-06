cba388ea089315db09640f6b0d9c447814c38fe3
KarlaNunes <karla.julyana1@gmail.com>
2023-12-06 15:03:26 -0300

    :hammer: mover views e urls para app de formulario

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from  core.views import home, index, meus_formularios, novo_formulario, tipo_questao
+from  core.views import home, index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
     path('home/', home),
-    path('',index),
-    path('meus-formularios/',meus_formularios),
-    path('novo-formulario/', novo_formulario),
-    path('tipo-questao', tipo_questao),
 ]
