eaf9d46cb5add154b5fadd552dcdf692a7eeb3f9
taysa-fernandes <taysamirelly34@gmail.com>
2023-12-08 16:30:10 -0300

    editando o alimento

--- alimento/urls.py
+++ alimento/urls.py
@@ -1,7 +1,8 @@
 
 from django.urls import path
-from  alimento.views import AlimentoCriar,AlimentoDeletar
+from  alimento.views import AlimentoCriar,AlimentoDeletar,AlimentoEditar
 urlpatterns = [
     path('cadastrar-alimento',AlimentoCriar.as_view(), name='cadastrar-alimento'),
-    path('alimentos/deletar/<int:id>',AlimentoDeletar.as_view(), name='deletar-alimento')
+    path('alimentos/deletar/<int:id>',AlimentoDeletar.as_view(), name='deletar-alimento'),
+    path('alimentos/editar/<int:id>',AlimentoEditar.as_view(), name='editar-alimento'),
 ]
