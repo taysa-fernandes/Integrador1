2e9b9c01fd8035982b9d974d6f42e90ec7f63d53
taysa-fernandes <taysamirelly34@gmail.com>
2023-12-08 11:57:51 -0300

    listando alimentos

--- alimento/urls.py
+++ alimento/urls.py
@@ -1,7 +1,6 @@
-from django.contrib import admin
+
 from django.urls import path
 from  alimento.views import AlimentoCriar
 urlpatterns = [
     path('cadastrar-alimento',AlimentoCriar.as_view(), name='cadastrar-alimento'),
-
 ]
