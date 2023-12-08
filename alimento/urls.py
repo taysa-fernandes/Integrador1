b991f3e2aeb14bfb31d23e32d51d11a885493bd9
taysa-fernandes <taysamirelly34@gmail.com>
2023-12-08 16:13:44 -0300

    alimento deletando

--- alimento/urls.py
+++ alimento/urls.py
@@ -1,6 +1,7 @@
 
 from django.urls import path
-from  alimento.views import AlimentoCriar
+from  alimento.views import AlimentoCriar,AlimentoDeletar
 urlpatterns = [
     path('cadastrar-alimento',AlimentoCriar.as_view(), name='cadastrar-alimento'),
+    path('alimentos/deletar/<int:id>',AlimentoDeletar.as_view(), name='deletar-alimento')
 ]
--- alimento/views.py
+++ alimento/views.py
@@ -14,4 +14,18 @@ class AlimentoListar(ListView):
     model = Alimento
     template_name = 'alimento/alimentos.html'
     context_object_name = 'alimentos'
+class AlimentoEditar(UpdateView):
+    model = Alimento
+    form_class = AlimentoForm
+    template_name = 'alimento/editaralimento.html'
+    success_url = reverse_lazy('listar-alimentos')
+    pk_url_kwarg = 'id'
+    
+class AlimentoDeletar(DeleteView):
+    model = Alimento
+    success_url = reverse_lazy('listar-alimentos')
+    pk_url_kwarg = 'id'
+    
+    def get(self, *args, **kwargs):
+        return self.delete(*args, **kwargs)
 
