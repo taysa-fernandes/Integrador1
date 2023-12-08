17e10c3b47a0afd4e9c11d884b8de3fef31aa9d1
taysa-fernandes <taysamirelly34@gmail.com>
2023-12-08 10:23:56 -0300

    correção de links laterais

--- alimento/urls.py
+++ alimento/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
-from  alimento.views import AlimentoCriar, AlimentoListar  
+from  alimento.views import AlimentoCriar
 urlpatterns = [
     path('cadastrar-alimento',AlimentoCriar.as_view(), name='cadastrar-alimento'),
-    path('alimentos/', AlimentoListar.as_view(), name='listar-alimentos'),
+
 ]
--- core/urls.py
+++ core/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from core.views import index
 from paciente.views import PacienteListar
+from alimento.views import AlimentoListar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
     path('home/', PacienteListar.as_view(), name='listar-pacientes'),
+    path('alimentos/', AlimentoListar.as_view(), name='listar-alimentos'),
 ]
