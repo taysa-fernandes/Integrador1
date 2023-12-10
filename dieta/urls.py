23d8712e7e59f2cc1c5064c6990e24b317154b5d
KarlaNunes <karla.julyana1@gmail.com>
2023-12-09 22:32:35 -0300

    :hammer: iniciar crud de dietas

--- dieta/urls.py
+++ dieta/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from  dieta.views import dieta, cadastrar_dieta
+from  dieta.views import dieta, cadastrar_dieta, listar_dietas, definir_numero_refeicoes
 
 urlpatterns = [
     path('cadastrar-dieta',cadastrar_dieta, name='cadastrar-dieta'),
-    path('dieta/',dieta, name='listar-dietas'),
+    path('ver-dieta/',dieta, name='ver-dieta'),
+    path('dieta/',listar_dietas, name='listar-dietas'),
+    path('definir-numero-refeicoes',definir_numero_refeicoes, name='definir-numero-refeicoes'),
 ]
--- dieta/views.py
+++ dieta/views.py
@@ -1,10 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from alimento.models import Alimento
+
 
 # Create your views here.
 
 def cadastrar_dieta(request):
-    return render(request, 'dieta/cadastrar-dieta.html')
+    numero_refeicoes = request.GET.get('numero_refeicoes', None)
+    numero_opcoes = request.GET.get('numero_opcoes', None)
+    
+    context = {
+        'numero_refeicoes': numero_refeicoes,
+        'numero_opcoes': numero_opcoes
+    }
+    
+    return render(request, 'dieta/cadastrar-dieta.html', context)
 
+
+def listar_dietas(request): 
+    return render(request, 'dieta/listar-dietas.html')
+    
+    
+def definir_numero_refeicoes(request): 
+    if request.method == 'POST':
+        numero_refeicoes = request.POST.get('numero-refeicoes')
+        alimentos = Alimento.objects.all()
+        
+        context = {
+            'numero_refeicoes': numero_refeicoes,
+            'alimentos': alimentos,
+        }
+        
+        return render(request, 'dieta/definir-numero-refeicoes.html', context)
+    
+    return render(request, 'dieta/definir-numero-refeicoes.html')
 
 def dieta(request):
     return render(request, 'dieta/visualizar-dieta.html')
