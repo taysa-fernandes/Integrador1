89770efc4fb9356064a987373c07e63e992b8699
KarlaNunes <karla.julyana1@gmail.com>
2023-12-10 14:49:00 -0300

    :tada: criação de dieta

--- dieta/models.py
+++ dieta/models.py
@@ -1,5 +1,21 @@
 from django.db import models
 
-class Dieta(models.Model):
-    numero_de_refeicoes = models.IntegerField()
+from alimento.models import Alimento
 
+class Dieta(models.Model):
+    STATUS_DIETA = (
+        ('pendente', 'Pendente'),
+        ('andamento', 'Em andamento'),
+        ('finalizada', 'Finalizada')
+    )
+    
+    nome = models.CharField(max_length=255)
+    status = models.CharField(max_length=50, choices=STATUS_DIETA, default='pendente')
+
+class Refeicao(models.Model):
+    nome = models.CharField(max_length=255)
+    dieta = models.ForeignKey(Dieta, on_delete=models.CASCADE, related_name='refeicoes')
+    
+class AlimentoRefeicao(models.Model):
+    alimento = models.ForeignKey(Alimento, on_delete=models.CASCADE)
+    refeicao = models.ForeignKey(Refeicao, on_delete=models.CASCADE)
--- dieta/views.py
+++ dieta/views.py
@@ -2,37 +2,61 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from alimento.models import Alimento
-
+from .models import Refeicao, Dieta, AlimentoRefeicao
+from django.views.generic import CreateView,UpdateView,ListView,DeleteView
 
 # Create your views here.
 
 def cadastrar_dieta(request):
-    numero_refeicoes = request.GET.get('numero_refeicoes', None)
-    numero_opcoes = request.GET.get('numero_opcoes', None)
+    dados_formulario = request.POST
     
-    context = {
-        'numero_refeicoes': numero_refeicoes,
-        'numero_opcoes': numero_opcoes
-    }
-    
-    return render(request, 'dieta/cadastrar-dieta.html', context)
+    for refeicao in Refeicao.objects.all():
+            nome_opcao = f'opcao_{refeicao.id}'
+            nome_substituto = f'substituto_{refeicao.id}'
+
+            opcao_selecionada = dados_formulario.get(nome_opcao)
+            substituto_selecionado = dados_formulario.get(nome_substituto)
+
+            # opção e substituto foram selecionados
+            if opcao_selecionada and substituto_selecionado:
+                alimento_opcao = Alimento.objects.get(nome=opcao_selecionada)
+                alimento_substituto = Alimento.objects.get(nome=substituto_selecionado)
+
+                AlimentoRefeicao.objects.create(alimento=alimento_opcao, refeicao=refeicao)
+                AlimentoRefeicao.objects.create(alimento=alimento_substituto, refeicao=refeicao)
+        
+        
+    return redirect('listar-dietas')
 
 
 def listar_dietas(request): 
-    return render(request, 'dieta/listar-dietas.html')
+    dietas = Dieta.objects.all()
+    
+    return render(request, 'dieta/listar-dietas.html', { 'dietas': dietas })
     
     
 def definir_numero_refeicoes(request): 
     if request.method == 'POST':
         numero_refeicoes = request.POST.get('numero-refeicoes')
+        numero_refeicoes_int = range(1, int(numero_refeicoes) + 1)
+        nome_dieta = request.POST.get('nome-dieta')
+        
+        dieta = Dieta.objects.create(nome=nome_dieta)
+        
+        refeicoes = []
+        
+        for num_refeicao in numero_refeicoes_int:
+            refeicao = Refeicao.objects.create(nome=f'Refeição {num_refeicao}', dieta=dieta)
+            refeicoes.append(refeicao)
+            
         alimentos = Alimento.objects.all()
         
         context = {
-            'numero_refeicoes': numero_refeicoes,
+            'refeicoes': refeicoes,
             'alimentos': alimentos,
         }
         
-        return render(request, 'dieta/definir-numero-refeicoes.html', context)
+        return render(request, 'dieta/cadastrar-dieta.html', context)
     
     return render(request, 'dieta/definir-numero-refeicoes.html')
 
