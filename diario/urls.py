238b732cdfe666c89448185f58ebb2c6c6f5ee51
KarlaNunes <karla.julyana1@gmail.com>
2023-12-10 17:48:01 -0300

    :tada: gerando diario com data dinamicamente

--- diario/urls.py
+++ diario/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
-from  diario.views import diario
+from  diario.views import Diario
 
 urlpatterns = [
-    path('diario/', diario, name="diario"),  
+    path('diario/', Diario.as_view(), name="diario"),  
 ]
--- diario/views.py
+++ diario/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from django.views import View
+from datetime import datetime
+import locale
 
 # Create your views here.
-def diario(request):
-    return render(request, 'diario/diarioAlimentar.html')
+class Diario(View):
+    
+    def get(self, request): 
+        locale.setlocale(locale.LC_TIME, 'pt_BR.utf-8')
+
+        data_atual = datetime.now()
+
+        dia_do_mes = data_atual.day
+        mes = data_atual.strftime("%B").capitalize()  # Nome do mês em português
+        dia_da_semana = data_atual.strftime("%A").capitalize()  # Dia da semana em português
+
+        context = {
+            'dia_do_mes': dia_do_mes,
+            'mes': mes,
+            'dia_da_semana': dia_da_semana,
+        }
+
+        return render(request, 'diario/diarioAlimentar.html', context)
