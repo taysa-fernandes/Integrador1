e3a3eef18ad0efc8f8fa11fdabeb0847769f8473
KarlaNunes <karla.julyana1@gmail.com>
2023-12-21 18:33:52 -0300

    :sparks: exibir modal de definir questoes

--- formulario/urls.py
+++ formulario/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from formulario.views import meus_formularios, novo_formulario, tipo_questao
+from formulario.views import meus_formularios, novo_formulario, tipo_questao, DefinirNumeroQuestoes
 
 urlpatterns = [
     path('meus-formularios/',meus_formularios, name='listar-formularios'),
-    path('novo-formulario/', novo_formulario, name='novo-formulario'),
+    path('novo-formulario/', novo_formulario, name='criar-formulario'),
     path('tipo-questao', tipo_questao, name='tipo-questao'),
+    path('definir-numero-questoes', DefinirNumeroQuestoes.as_view(), name='definir-numero-questoes'),
 ]
--- formulario/views.py
+++ formulario/views.py
@@ -1,9 +1,68 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.views import View
 # Create your views here.
+
+class DefinirNumeroQuestoes(View):
+    template_name = 'formulario/definir-numero-questoes.html'
+    
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+class CriarFormulario(View):
+    template_name_create = 'dieta/criar-formulario.html' 
+    template_name_process = 'dieta/definir-numero-questoes.html'
+
+    def get(self, request, *args, **kwargs):
+        return redirect('definir-numero-questoes')
+    
+    def post(self, request, *args, **kwargs):
+        dados_formulario = request.POST
+        formulario_id = dados_formulario.get('formulario_id')
+        
+        if formulario_id is None:
+            # Lógica para criar a dieta
+            numero_questoes = dados_formulario.get('numero-refeicoes')
+            nome_dieta = dados_formulario.get('nome-dieta')
+
+            if numero_questoes and nome_dieta:
+                numero_questoes_int = range(1, int(numero_questoes) + 1)            
+                dieta = Dieta.objects.create(nome=nome_dieta)
+                refeicoes = [Refeicao.objects.create(nome=f'Refeição {num_refeicao}', dieta=dieta)
+                             for num_refeicao in numero_questoes_int]
+                alimentos = Alimento.objects.all()
+
+                context = {
+                    'refeicoes': refeicoes,
+                    'alimentos': alimentos,
+                    'dieta': dieta
+                }
+
+                return render(request, self.template_name_create, context)
+            
+        dieta = get_object_or_404(Dieta, id=dieta_id)
+        # Lógica para processar o formulário da dieta existente
+        for refeicao in dieta.refeicoes.all():
+            nome_opcao = f'opcao_{refeicao.id}'
+            nome_substituto = f'substituto_{refeicao.id}'
+            
+            print('dados do formulario de criar: ', dados_formulario)
+            
+            opcao_selecionada = dados_formulario.get(nome_opcao)
+            substituto_selecionado = dados_formulario.get(nome_substituto)
+
+            # opção e substituto foram selecionados
+            if opcao_selecionada and substituto_selecionado:
+                alimento_opcao = Alimento.objects.get(nome=opcao_selecionada)
+                alimento_substituto = Alimento.objects.get(nome=substituto_selecionado)
+
+                refeicao.alimentos.set([alimento_opcao, alimento_substituto])
+
+        return redirect('listar-dietas')
 
 def meus_formularios(request):
     return render(request, 'formulario/listar-formularios.html')
+
+
 
 def novo_formulario(request):
     return render(request, 'formulario/novo-formulario.html')
