e99b0e76e076a1143ee856803630fe15841f6b2e
KarlaNunes <karla.julyana1@gmail.com>
2023-12-21 10:40:14 -0300

    :hammer: mover logica de criar dieta para uma unica view

--- dieta/urls.py
+++ dieta/urls.py
@@ -4,7 +4,7 @@ from  dieta.views import dieta, CadastrarDieta, ListarDietas, EditarDieta, Delet
 
 urlpatterns = [
     path('cadastrar-dieta', CadastrarDieta.as_view(), name='cadastrar-dieta'),
-    path('editar-dieta/<int:id>/', EditarDieta.as_view(), name='editar-dieta'),
+    path('editar-dieta/<int:dieta_id>/', EditarDieta.as_view(), name='editar-dieta'),
     path('dieta/',ListarDietas.as_view(), name='listar-dietas'),
     path('deletar-dieta/<int:id>/', DeletarDieta.as_view(), name='deletar-dieta'),
     path('ver-dieta/',dieta, name='ver-dieta'),
--- dieta/views.py
+++ dieta/views.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.shortcuts import get_object_or_404, render, redirect
 from django.http import HttpResponseRedirect
 from django.views import View
@@ -5,17 +6,44 @@ from django.urls import reverse_lazy
 from alimento.models import Alimento
 from .models import Refeicao, Dieta
 from django.views.generic import CreateView,UpdateView,ListView,DeleteView
+from .forms import DietaForm
 
 # Create your views here.
 
 class CadastrarDieta(View):
-    # template_name = 'dieta/cadastrar-dieta.html' 
+    template_name_create = 'dieta/cadastrar-dieta.html' 
+    template_name_process = 'dieta/definir-numero-refeicoes.html'
+
+    def get(self, request, *args, **kwargs):
+        return redirect('definir-numero-refeicoes')
     
-    def post(self, request):
+    def post(self, request, *args, **kwargs):
         dados_formulario = request.POST
         dieta_id = dados_formulario.get('dieta_id')
-        dieta = get_object_or_404(Dieta, id=dieta_id)
         
+
+        if dieta_id is None:
+            # Lógica para criar a dieta
+            numero_refeicoes = dados_formulario.get('numero-refeicoes')
+            nome_dieta = dados_formulario.get('nome-dieta')
+
+            if numero_refeicoes and nome_dieta:
+                numero_refeicoes_int = range(1, int(numero_refeicoes) + 1)            
+                dieta = Dieta.objects.create(nome=nome_dieta)
+                refeicoes = [Refeicao.objects.create(nome=f'Refeição {num_refeicao}', dieta=dieta)
+                             for num_refeicao in numero_refeicoes_int]
+                alimentos = Alimento.objects.all()
+
+                context = {
+                    'refeicoes': refeicoes,
+                    'alimentos': alimentos,
+                    'dieta': dieta
+                }
+
+                return render(request, self.template_name_create, context)
+            
+        dieta = get_object_or_404(Dieta, id=dieta_id)
+        # Lógica para processar o formulário da dieta existente
         for refeicao in dieta.refeicoes.all():
             nome_opcao = f'opcao_{refeicao.id}'
             nome_substituto = f'substituto_{refeicao.id}'
@@ -29,8 +57,7 @@ class CadastrarDieta(View):
                 alimento_substituto = Alimento.objects.get(nome=substituto_selecionado)
 
                 refeicao.alimentos.set([alimento_opcao, alimento_substituto])
-            
-            
+
         return redirect('listar-dietas')
 
 
@@ -40,38 +67,32 @@ class ListarDietas(ListView):
     context_object_name = 'dietas'
     
 class DefinirNumeroRefeicoes(View):
-    template_name = 'dieta/definir-numero-refeicoes.html' 
-    success_url = 'cadastrar-dieta'
-
-    def get(self, request):
-        return render(request, self.template_name)
-
-    def post(self, request):
-        numero_refeicoes = request.POST.get('numero-refeicoes')
-        nome_dieta = request.POST.get('nome-dieta')
-
-        if numero_refeicoes and nome_dieta:
-            numero_refeicoes_int = range(1, int(numero_refeicoes) + 1)            
-            dieta = Dieta.objects.create(nome=nome_dieta)
-            refeicoes = [Refeicao.objects.create(nome=f'Refeição {num_refeicao}', dieta=dieta)
-                         for num_refeicao in numero_refeicoes_int]
-            alimentos = Alimento.objects.all()
-            
-            context = {
-                'refeicoes': refeicoes,
-                'alimentos': alimentos,
-                'dieta': dieta
-            }
-
-            return render(request, 'dieta/cadastrar-dieta.html', context)
-
+    template_name = 'dieta/definir-numero-refeicoes.html'
+    
+    def get(self, request, *args, **kwargs):
         return render(request, self.template_name)
 
 class EditarDieta(UpdateView):
-    model = Refeicao
-    template_name = 'dieta/cadastrar-dieta.html'
-    pk_url_kwarg = 'id'
+    model = Dieta
+    template_name = 'dieta/editar-dieta.html'
+    form_class = DietaForm  # Use o seu formulário personalizado
+    pk_url_kwarg = 'dieta_id'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        dieta = self.get_object()
+        
+        context['dieta'] = dieta
+        context['refeicoes'] = dieta.refeicoes.all()
+        
+        alimentos =  Alimento.objects.filter(refeicoes__dieta=dieta).distinct()
+        
+        context['alimentos'] = alimentos
+        
+        print('alimentos na view: ', alimentos)
 
+        return context
+    
     def get_success_url(self):
         return reverse_lazy('listar-dietas') 
 
