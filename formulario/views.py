e3452df20af3c8f5b13d16da499fedb1870ae56a
KarlaNunes <karla.julyana1@gmail.com>
2023-12-27 10:27:01 -0300

    :art: paginacao de 3

--- formulario/views.py
+++ formulario/views.py
@@ -65,7 +65,7 @@ class ListarFormularios(NutricionistaMixin, ListView):
     model = Formulario
     template_name = 'formulario/listar-formularios.html'
     context_object_name = 'formularios'
-    paginate_by = 5
+    paginate_by = 3
     
 class EditarFormulario(NutricionistaMixin, UpdateView):
     model = Formulario
