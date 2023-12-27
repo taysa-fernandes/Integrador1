8551963f2bb96f86581b7db4120804aaef0a9ceb
KarlaNunes <karla.julyana1@gmail.com>
2023-12-27 13:29:57 -0300

    :art: ajustar lista de substitutos

--- alimento/views.py
+++ alimento/views.py
@@ -14,7 +14,27 @@ class AlimentoListar(NutricionistaMixin, ListView):
     model = Alimento
     template_name = 'alimento/alimentos.html'
     context_object_name = 'alimentos'
-    paginate_by = 3
+    paginate_by = 3 
+
+    def get(self, request, *args, **kwargs):
+        # Obter o valor do parâmetro 'tipo' da URL ou usar 'comuns' como padrão
+        tipo_alimento = self.request.GET.get('tipo', 'comuns')
+
+        # Validar o valor do parâmetro, se necessário
+        # Você pode adicionar lógica para garantir que o valor seja válido
+
+        # Armazenar o valor na instância da classe para que ele possa ser usado em outros métodos
+        self.tipo_alimento = tipo_alimento
+
+        return super().get(request, *args, **kwargs)
+
+    def get_queryset(self):
+        # Filtrar os alimentos com base no valor do parâmetro 'tipo'
+        if self.tipo_alimento == 'substitutos':
+            return Alimento.objects.filter(e_substituto=True)
+        else: 
+            return Alimento.objects.filter(e_substituto=False)
+        
 class AlimentoEditar(NutricionistaMixin, UpdateView):
     model = Alimento
     form_class = AlimentoForm
